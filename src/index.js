a37a4e1748d31253f955516b0c7440173c73dd71
Roman Fairushyn <fyrushin_roman@yahoo.com>
2021-04-20 17:45:18 +0300

    snap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './styles.css'
-import {createStore} from './createStore'
+import {applyMiddleware, createStore} from 'redux'
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
 import {rootReducer} from './redux/rootReducer'
+import {asyncIncrement, decrement, increment} from "./redux/actions";
 
 let counter = document.getElementById('counter');
 const addBtn = document.getElementById('add');
@@ -8,18 +11,35 @@ const subBtn = document.getElementById('sub');
 const asyncBtn = document.getElementById('async');
 const themeBtn = document.getElementById('theme');
 
-const store = createStore(rootReducer, 0);
+
+// function logger(state) {
+//     return function (next) {
+//         return function (action) {
+//             console.log('Prev State:', state.getState())
+//             console.log('Action:', action)
+//             const newState = next(action)
+//             console.log('Prev State:', newState)
+//             return newState;
+//         }
+//     }
+// }
+
+
+const store = createStore(
+    rootReducer,
+    0,
+    applyMiddleware(thunk, logger));
 
 addBtn.addEventListener('click', () => {
-    store.dispatch({type: 'INCREMENT'});
+    store.dispatch(increment());
 });
 
 subBtn.addEventListener('click', () => {
-    store.dispatch({type: 'DECREMENT'});
+    store.dispatch(decrement());
 });
 
 asyncBtn.addEventListener('click', () => {
-
+    store.dispatch(asyncIncrement());
 });
 
 store.subscribe(() => {
